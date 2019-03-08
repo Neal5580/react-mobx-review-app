8b22f05486c118a28e031dcc169d3becb233b4f6
Neal Shen <shenyijie5580@gmail.com>
2019-03-08 12:51:13 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { decorate, observable, action, computed } from "mobx";
+import Store from "./Store";
+import Form from "./components/Form";
+import Dashboard from "./components/Dashboard";
+import Reviews from "./components/Reviews";
+
+decorate(Store, {
+    reviewList: observable,
+    addReview: action,
+    averageScore: computed,
+    reviewCount: computed
+});
+
+const reviewStore = new Store();
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container">
+                <Form store={reviewStore} />
+                <Dashboard store={reviewStore} />
+                <Reviews store={reviewStore} />
+            </div>
+        );
+    }
 }
 
 export default App;
